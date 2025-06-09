f2a74773d7ee859d5f2c59845c2c01f0a0b4e8e5
Bulat Khunkaev <bulatkhunkaev@gmail.com>
2025-06-09 22:52:04 +0300

    fix: show MainPage on default / route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 // src/App.js
-import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import PromoPage from './pages/PromoPage';
-
-function RedirectToMain() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname === '/') {
-      navigate('/main');
-    }
-  }, [location, navigate]);
-
-  return null;
-}
 
 function App() {
   return (
     <Router>
-      <RedirectToMain />
       <nav style={{ padding: 10 }}>
-        <Link to="/main">Главная</Link> | <Link to="/promo">Промо</Link>
+        <Link to="/">Главная</Link> | <Link to="/promo">Промо</Link>
       </nav>
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/promo" element={<PromoPage />} />
       </Routes>
