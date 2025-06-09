29fead2ad4f5d66028d6ca15447d64f110c6477e
Bulat Khunkaev <bulatkhunkaev@gmail.com>
2025-06-09 22:39:59 +0300

    fix: redirect from / to /main in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// src/App.js
+import React, { useEffect } from 'react';
+import { HashRouter as Router, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import PromoPage from './pages/PromoPage';
+
+function RedirectToMain() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/main');
+    }
+  }, [location, navigate]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <RedirectToMain />
       <nav style={{ padding: 10 }}>
-        <Link to="/">Главная</Link> | <Link to="/promo">Промо</Link>
+        <Link to="/main">Главная</Link> | <Link to="/promo">Промо</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<MainPage />} />     {/* Главная по умолчанию */}
+        <Route path="/main" element={<MainPage />} />
         <Route path="/promo" element={<PromoPage />} />
       </Routes>
     </Router>
