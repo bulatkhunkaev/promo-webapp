52753a647134a6ca10f1a548ba6b4947827fe1b8
Bulat Khunkaev <bulatkhunkaev@gmail.com>
2025-06-09 23:30:31 +0300

    Вернул навигацию Главная и Промо

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useLocation, Link } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import PromoPage from './pages/PromoPage';
 
 function RedirectUnknownToMain() {
   const location = useLocation();
-
-  // Если путь не / и не /promo, перенаправляем на /
   if (location.pathname !== '/' && location.pathname !== '/promo') {
     return <Navigate to="/" replace />;
   }
   return null;
 }
 
+function Navigation() {
+  return (
+    <nav style={{ padding: 10, textAlign: 'center' }}>
+      <Link to="/" style={{ marginRight: 10 }}>Главная</Link>
+      <Link to="/promo">Промо</Link>
+    </nav>
+  );
+}
+
 function App() {
   return (
     <Router>
       <RedirectUnknownToMain />
+      <Navigation />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/promo" element={<PromoPage />} />
